8f503208d4a2d2826fb920421f3591b562b2d6d2
Lucasliuzao <lucasisrael85@gmail.com>
2024-03-04 15:15:33 -0300

    salvar alteracao

--- 185- [JAWS] -Atividade — Trabalhar com o Amazon S3/Creating and initializing the S3 share bucket.sh
+++ 185- [JAWS] -Atividade — Trabalhar com o Amazon S3/Creating and initializing the S3 share bucket.sh
@@ -1,5 +1,5 @@
-aws s3 mb s3://<cafe-xxxnnn> --region 'us-west-2'
+aws s3 mb s3://cafe-xxxnnn --region 'us-west-2'
 
-aws s3 sync ~/initial-images/ s3://<cafe-xxxnnn>/images
+aws s3 sync ~/initial-images/ s3://cafe-xxxnnn/images
 
-aws s3 ls s3://<cafe-xxxnnn>/images/ --human-readable --summarize
+aws s3 ls s3://cafe-xxxnnn/images/ --human-readable --summarize
